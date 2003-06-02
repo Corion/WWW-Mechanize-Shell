c9196ce1c89bac8a590c2f80e7a4c11740edac33
corion <corion>
2003-06-02 17:36:52 +0000

    Linux compatibility fixes

--- lib/HTML/Display.pm
+++ lib/HTML/Display.pm
@@ -11,24 +11,26 @@ HTML::Display - display HTML locally in a browser
 
 =for example begin
 
+  use strict;
   use HTML::Display;
 
   # guess the best value from $ENV{PERL_HTML_DISPLAY_CLASS}
   # or $ENV{PERL_HTML_DISPLAY_COMMAND}
   # or the operating system, in that order
-  my $browser = HTML::Display->new(); 
+  my $browser = HTML::Display->new();
+  my $location = "http://www.google.com/";
   $browser->display(html => $html, location => $location);
 
   # Or, for a one-off job :
-  display("<html><body><h1>Hello world!</h1></body></html>");  
-  
+  display("<html><body><h1>Hello world!</h1></body></html>");
+
 =for example end
 
 =for example_testing
   %os_default = ();
   $html = "foo\n";
   is($_STDOUT_,"foo\n<html><body><h1>Hello world!</h1></body></html>");
-  
+
 =head1 DESCRIPTION
 
 This module abstracts the task of displaying HTML to the user. The
@@ -41,17 +43,17 @@ if it dosen't find a good browser, you can modify the behaviour by
 setting some environment variables :
 
   PERL_HTML_DISPLAY_CLASS
-  
+
 If HTML::Display already provides a class for the browser you want to
 use, setting C<PERL_HTML_DISPLAY_CLASS> to the name of the class will
 make HTML::Display use that class instead of what it detects.
 
   PERL_HTML_DISPLAY_COMMAND
-  
+
 If there is no specialized class yet, but your browser can be controlled
 via the command line, then setting C<PERL_HTML_DISPLAY_COMMAND> to the
 string to navigate to the URL will make HTML::Display use a C<system()>
-call to the string. A C<%s> in the value will be replaced with the name 
+call to the string. A C<%s> in the value will be replaced with the name
 of the temporary file containing the HTML to display.
 
 =cut
@@ -65,14 +67,14 @@ require Exporter;
 =head2 %HTML::Display::os_default
 
 The hash C<%HTML::Display::os_default> contains pairs of class names
-for the different operating systems and routines that test whether 
+for the different operating systems and routines that test whether
 this script is currently running under it. If you you want to dynamically
 add a new class or replace a class (or the rule), modify C<%os_default> :
 
 =for example begin
 
   # Install class for MagicOS
-  $HTML::Display::os_default{"HTML::Display::MagicOS"} 
+  $HTML::Display::os_default{"HTML::Display::MagicOS"}
     = sub { $^O =~ qr/magic/i };
 
 =for example end
@@ -88,13 +90,13 @@ add a new class or replace a class (or the rule), modify C<%os_default> :
 =head2 __PACKAGE__->new %ARGS
 
 =cut
-  
+
 sub new {
   my ($class,%args) = @_;
-  
+
   # First see whether the programmer or user specified a class
   my $best_class = delete $args{class} || $ENV{PERL_HTML_DISPLAY_CLASS};
-  
+
   # Now, did they specify a command?
   unless ($best_class) {
     my $command = delete $args{browsercmd} || $ENV{PERL_HTML_DISPLAY_COMMAND};
@@ -104,10 +106,10 @@ sub new {
     };
   };
 
-  unless ($best_class) {  
+  unless ($best_class) {
     for my $class (sort keys %os_default) {
       #print "Looking at $class\n";
-      $best_class = $class 
+      $best_class = $class
         if $os_default{$class}->();
     };
   };
@@ -115,7 +117,7 @@ sub new {
 
   eval "require $best_class";
   die $@ if $@;
-  return $best_class->new(@_);  
+  return $best_class->new(@_);
 };
 
 =head2 $browser-E<gt>display( %ARGS )
@@ -130,7 +132,7 @@ Will display the HTML. The following arguments are valid :
 If only one argument is passed, then it is taken as if
 
   html => $_[0]
-  
+
 was passed.
 
 =cut
@@ -148,7 +150,7 @@ sub display {
 =head1 EXPORTS
 
 The subroutine C<display> is exported by default
-  
+
 =head1 COMMAND LINE USAGE
 
 Display some HTML to the user :
@@ -161,9 +163,9 @@ Display a web page to the user :
 
 Display the same page with the images also working :
 
-  perl -MLWP::Simple -MHTML::Display -e "display html => get('http://www.google.com'), 
+  perl -MLWP::Simple -MHTML::Display -e "display html => get('http://www.google.com'),
                                                  location => 'http://www.google.com'"
-  
+
 =cut
 
 1;
